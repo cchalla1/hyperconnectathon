5ae787baf21d0dbaa38acda80ce83005b382b02c
cchalla1 <chandrakanth.cck@gmail.com>
2019-12-03 19:15:49 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,35 @@ const app = express();
 const bodyParser = require('body-parser');
 const http = require('http');
 const server = http.createServer(app);
-const io = require('socket.io')(app);
+// const io = require('socket.io')(server);
+const cors = require('cors');
+app.use(cors({ origin: '*' }));
+// Settings for CORS
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.header('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.header('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', false);
+
+    // Pass to next layer of middleware
+    next();
+});
+
+var io = require('socket.io').listen(server, {
+  log: false,
+  agent: false,
+  origins: '*:*',
+  transports: ['websocket', 'htmlfile', 'xhr-polling', 'jsonp-polling', 'polling']
+});
 // io.configure(function () { 
 //   io.set("transports", ["xhr-polling"]); 
 //   io.set("polling duration", 10); 
