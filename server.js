72deec4142e18afc7a841c3745b045cf42a02312
cchalla1 <chandrakanth.cck@gmail.com>
2019-12-03 20:03:19 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,54 +1,35 @@
+var WebSocketServer = require('websocket').server;
+var http = require('http');
+const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser');
-const http = require('http');
-const server = http.createServer(app);
-// const io = require('socket.io')(server);
-const cors = require('cors');
-app.use(cors({ origin: '*' }));
-// Settings for CORS
-app.use(function (req, res, next) {
+let webSocket;
 
-    // Website you wish to allow to connect
-    res.header('Access-Control-Allow-Origin', '*');
+var server = http.createServer(app);
 
-    // Request methods you wish to allow
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.header('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', false);
-
-    // Pass to next layer of middleware
-    next();
+// create the server
+const WebSocket = require('ws');
+ 
+const wss = new WebSocket.Server({ server });
+ 
+wss.on('connection', function connection(ws) {
+  ws.on('message', function incoming(message) {
+    console.log('received: %s', message);
+  });
+  webSocket = ws;
+ 
+  ws.send('something');
 });
-
-var io = require('socket.io').listen(server, {
-  log: false,
-  agent: false,
-  origins: '*:*',
-  transports: ['websocket', 'htmlfile', 'xhr-polling', 'jsonp-polling', 'polling']
-});
-// io.configure(function () { 
-//   io.set("transports", ["xhr-polling"]); 
-//   io.set("polling duration", 10); 
-// });
-io.on('connection', (socket) => {
-  console.log('Client connected');
-  socket.on('disconnect', () => console.log('Client disconnected'));
-});
-setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
-const port = 8000
 
 app.use(bodyParser.json());
-app.get('/', (req, res) => res.send('Hello World!'))
-app.post('/stream', function(req, res) {
-  io.emit('data', req.body);
-  console.log(req.body);
-  res.status(200).json(req.body);
+
+app.get('/', function(req, res) {
+  res.send("Hellow World");
 });
 
-app.listen(process.env.PORT || port, () => console.log(`Example app listening on port ${port}!`))
+app.post("/stream", function(req, res) {
+  webSocket.send(JSON.stringify(req.body));
+  res.status(200).json(req.body);
+})
+
+server.listen(process.env.PORT || 8000, function() { });
