225d11efb1d24b1b98e804962ad4112da2bea4a1
cchalla1 <chandrakanth.cck@gmail.com>
2019-12-03 18:41:12 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,8 +4,16 @@ const bodyParser = require('body-parser');
 const http = require('http');
 const server = http.createServer(app);
 const io = require('socket.io').listen(server);
-io.on('connection', socket => {
-  console.log("user connected");
+io.configure(function () { 
+  io.set("transports", ["xhr-polling"]); 
+  io.set("polling duration", 10); 
+});
+io.sockets.on('connection', function (socket) {
+  io.sockets.emit('status', { status: status }); // note the use of io.sockets to emit but socket.on to listen
+  socket.on('reset', function (data) {
+    status = "War is imminent!";
+    io.sockets.emit('status', { status: status });
+  });
 });
 const port = 8000
 
