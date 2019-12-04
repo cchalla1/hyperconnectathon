d209e5820226ed4f92b83cd0a060e1d99536abd0
cchalla1 <chandrakanth.cck@gmail.com>
2019-12-04 15:39:46 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,16 +9,11 @@ const server = http.createServer(app);
 const WebSocket = require('ws');
  
 const wss = new WebSocket.Server({ server });
-let clients = new Map();
  
 wss.on('connection', function connection(ws, req, client) {
   ws.on('message', function incoming(message) {
     console.log('received: %s', message);
   });
-
-  const ip = req.connection.remoteAddress;
-  console.log(req.connection, ip);
-  clients.set(ip, client);
 });
 
 app.use(bodyParser.json());
@@ -28,20 +23,20 @@ app.get('/', function(req, res) {
 });
 
 app.post("/stream", (req, res) => {
-  for (const client in clients) {
+  wss.clients.forEach(client => {
     if (client.readyState === WebSocket.OPEN) {
       client.send(JSON.stringify(req.body));
     }
-  }
+  });
   res.status(200).json(req.body);
 })
 
 app.post("/stream/addToCart", (req, res) => {
-  for (const client in clients) {
+  wss.clients.forEach(client => {
     if (client.readyState === WebSocket.OPEN) {
       client.send(JSON.stringify(req.body));
     }
-  }
+  });
   res.status(200).json(req.body);
 })
 
