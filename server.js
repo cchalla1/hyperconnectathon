9c56f7d581a2d32e2e7d4a6d8cc85afe3079dc7e
cchalla1 <chandrakanth.cck@gmail.com>
2019-12-04 15:19:41 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,24 +1,25 @@
-//var WebSocketServer = require('websocket').server;
+var WebSocketServer = require('websocket').server;
 var http = require('http');
 const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
-let webSocket;
 
-var server = http.createServer(app);
+const server = http.createServer(app);
 
 // create the server
 const WebSocket = require('ws');
  
 const wss = new WebSocket.Server({ server });
+let clients = new Map();
  
-wss.on('connection', function connection(ws) {
+wss.on('connection', function connection(ws, req, client) {
   ws.on('message', function incoming(message) {
     console.log('received: %s', message);
   });
-  webSocket = ws;
- 
-  ws.send('something');
+
+  const ip = req.connection.remoteAddress;
+  console.log(req.connection, ip);
+  clients.set(ip, client);
 });
 
 app.use(bodyParser.json());
@@ -27,11 +28,28 @@ app.get('/', function(req, res) {
   res.send("Hellow World");
 });
 
-app.post("/stream", function(req, res) {
-  webSocket.send(JSON.stringify(req.body));
+app.post("/stream", (req, res) => {
+  for (const client in clients) {
+    clients.forEach(client => {
+      console.log(Object.keys(client), client._events);
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(JSON.stringify(req.body));
+      }
+    });
+  }
   res.status(200).json(req.body);
 })
 
-server.listen(process.env.PORT || 8000, function() {
-  console.log('server started');
-});
+app.post("/stream/addToCart", (req, res) => {
+  for (const client in clients) {
+    clients.forEach(client => {
+      console.log(Object.keys(client), client._events);
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(JSON.stringify(req.body));
+      }
+    });
+  }
+  res.status(200).json(req.body);
+})
+
+server.listen(process.env.PORT || 8000, function() { });
